c1fd810258b6b5c28a19cdaf07cc5f67dd0ad6e5
LEIF-2\razan <somethingrazan@gmail.com>
2017-12-13 17:58:53 -0500

    Mini modif

--- Dungeon/main.cpp
+++ Dungeon/main.cpp
@@ -7,13 +7,6 @@
 
 void main()
 {
-	//Creating the map
-	Dungeon test;
-
-	test.init(25, 15, 15, 50, 50);
-	test.createRooms();
-	test.createHallway();
-
 	//Creatinp the window of the map
 	windowMap window;
 	window.createMap(20, 15, 15, 75, 75);
